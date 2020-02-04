e3c5b91f8478cdf92d6a4efe167cfebcb59b070d
hoangnguyen177 <hoangnguyen177@gmail.com>
2020-02-04 14:45:01 +1000

    first commit

---
+++ src/main/java/au/org/rcc/keycloak/authentication/LinkIdpLoginAuthenticator.java
@@ -0,0 +1,201 @@
+package au.org.rcc.keycloak.authentication;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Hashtable;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
+import javax.naming.directory.SearchControls;
+import javax.naming.directory.SearchResult;
+
+import org.jboss.logging.Logger;
+import org.keycloak.authentication.AuthenticationFlowContext;
+import org.keycloak.authentication.AuthenticationFlowError;
+
+import org.keycloak.authentication.authenticators.broker.AbstractIdpAuthenticator;
+import org.keycloak.authentication.authenticators.broker.util.ExistingUserInfo;
+import org.keycloak.authentication.authenticators.broker.util.SerializedBrokeredIdentityContext;
+import org.keycloak.broker.provider.BrokeredIdentityContext;
+import org.keycloak.models.AuthenticatorConfigModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.UserModel;
+
+/**
+ * @author Hoang Nguyen
+ */
+public class LinkIdpLoginAuthenticator extends AbstractIdpAuthenticator {
+    
+	private DirContext ctx = null;
+	private static Logger logger = Logger.getLogger(LinkIdpLoginAuthenticator.class);
+	
+    protected String getUsername(AuthenticationFlowContext context,
+    		BrokeredIdentityContext brokerContext) {
+        RealmModel realm = context.getRealm();
+        return realm.isRegistrationEmailAsUsername() ? brokerContext.getEmail() : brokerContext.getModelUsername();
+    }
+    
+    // Could be overriden to detect duplication based on other criterias (firstName, lastName, ...)
+    protected ExistingUserInfo checkExistingUser(AuthenticationFlowContext context, String username, SerializedBrokeredIdentityContext serializedCtx, BrokeredIdentityContext brokerContext) {
+
+        if (brokerContext.getEmail() != null && !context.getRealm().isDuplicateEmailsAllowed()) {
+            UserModel existingUser = context.getSession().users().getUserByEmail(brokerContext.getEmail(), context.getRealm());
+            if (existingUser != null) {
+                return new ExistingUserInfo(existingUser.getId(), UserModel.EMAIL, existingUser.getEmail());
+            }
+        }
+
+        UserModel existingUser = context.getSession().users().getUserByUsername(username, context.getRealm());
+        if (existingUser != null) {
+            return new ExistingUserInfo(existingUser.getId(), UserModel.USERNAME, existingUser.getUsername());
+        }
+
+        return null;
+    }
+    
+    
+
+    public boolean requiresUser() {
+        return false;
+    }
+
+    public boolean configuredFor(KeycloakSession session, RealmModel realm, UserModel user) {
+        return true;
+    }
+
+    @Override
+	protected void actionImpl(AuthenticationFlowContext context, 
+			SerializedBrokeredIdentityContext serializedCtx, BrokeredIdentityContext brokerContext) {		
+	}
+
+
+	@Override
+	protected void authenticateImpl(AuthenticationFlowContext context, 
+			SerializedBrokeredIdentityContext serializedCtx, BrokeredIdentityContext brokerContext) {
+		if (context.getAuthenticationSession().getAuthNote(EXISTING_USER_INFO) != null) {
+            context.attempted();
+            return;
+        }
+
+        String username = getUsername(context, brokerContext);
+        if (username == null) {
+            context.getAuthenticationSession().setAuthNote(ENFORCE_UPDATE_PROFILE, "true");
+            context.resetFlow();
+            return;
+        }
+
+        ExistingUserInfo duplication = checkExistingUser(context, username, serializedCtx, brokerContext);
+
+        if (duplication == null) {
+        	System.out.println(">>>>>>>>>>User does not exist. New user is not being created.");
+        	context.failure(AuthenticationFlowError.UNKNOWN_USER);
+        } else {
+            // Set duplicated user, so next authenticators can deal with it
+            context.getAuthenticationSession().setAuthNote(EXISTING_USER_INFO, duplication.serialize());
+            UserModel existingUser = context.getSession().users().getUserByEmail(brokerContext.getEmail(), context.getRealm());
+            String baseDn = context.getAuthenticatorConfig().getConfig().get(LinkIdpLoginAuthenticatorFactory.LDAP_SECURITY_BASE_DN);
+            try {
+        		List<String> clusters = getClusterAccess(context, baseDn, existingUser.getUsername());
+        		System.out.println(">>>>>>>>>>>>>[LinkIdpLoginAuthneticator]clusters:" + clusters.toString());
+        		existingUser.setAttribute("clusters", clusters);
+				context.setUser(existingUser);
+	        	context.success();
+			} catch (NamingException e) {
+				System.out.println("Internal error:" + e.getMessage());
+				context.failure(AuthenticationFlowError.INTERNAL_ERROR);        
+			}
+            
+            context.setUser(existingUser);
+            context.success();
+        }
+		
+	}
+    
+    
+    private List<String> getClusterAccess(AuthenticationFlowContext context, 
+    		String baseDn, String username) 
+    		throws NamingException {
+    	String clusterNamesStr = context.getAuthenticatorConfig().getConfig().get(LinkIdpLoginAuthenticatorFactory.CLUSTER_NAMES);
+		String[] clusterNames = clusterNamesStr.split(";");
+        List<String> clusters = new ArrayList<String>(Arrays.asList(clusterNames));
+        if(this.ctx == null)
+    		this.initLdapContext(context);
+		String searchFilter = String.format("(cn=%s)", escapeLDAPSearchFilter(username));
+		NamingEnumeration<SearchResult> results = 
+				ctx.search(baseDn, searchFilter, new SearchControls());
+		while(results.hasMore()) {
+			SearchResult result = results.next();
+			String dn = result.getAttributes().get("dn").toString();
+			for(String name: clusterNames) {
+				if(dn.contains(name))
+					clusters.add(name);
+			}
+		}
+		return clusters;
+    }
+    
+    /**
+     * create ldap context
+     * @param context
+     * @throws NamingException
+     */
+    private void initLdapContext(AuthenticationFlowContext context) throws NamingException{
+    	AuthenticatorConfigModel config = context.getAuthenticatorConfig();
+    	String ldapProviderUrl = config.getConfig().get(LinkIdpLoginAuthenticatorFactory.LDAP_SERVER_URL);
+    	String ldapSecurityPrinciple = config.getConfig().get(LinkIdpLoginAuthenticatorFactory.LDAP_SECURITY_PRINCIPLE);
+    	String ldapSecurityAuthentication= config.getConfig().get(LinkIdpLoginAuthenticatorFactory.LDAP_SECURITY_AUTHENTICATION);
+    	String ldapSecurityCredentials= config.getConfig().get(LinkIdpLoginAuthenticatorFactory.LDAP_SECURITY_CREDENTIALS);
+    	
+		Hashtable<String, String> env = new Hashtable<String, String>();
+		env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+		env.put(Context.PROVIDER_URL, ldapProviderUrl);
+
+		// Authenticate if credentials were provided
+		if (ldapSecurityPrinciple != null) {
+			env.put(Context.SECURITY_AUTHENTICATION, ldapSecurityAuthentication);
+			env.put(Context.SECURITY_PRINCIPAL, ldapSecurityPrinciple);
+			env.put(Context.SECURITY_CREDENTIALS, ldapSecurityCredentials);
+		}
+		else
+			throw new NamingException("Ldap Principle must not be null");
+		ctx = new InitialDirContext(env);
+	}
+    
+    /**
+	 * Prevent LDAP injection
+	 * @param filter LDAP filter string to escape
+	 * @return escaped string
+	 */
+	private String escapeLDAPSearchFilter(String filter) {
+	       StringBuilder sb = new StringBuilder();
+	       for (int i = 0; i < filter.length(); i++) {
+	           char curChar = filter.charAt(i);
+	           switch (curChar) {
+	               case '\\':
+	                   sb.append("\\5c");
+	                   break;
+	               case '*':
+	                   sb.append("\\2a");
+	                   break;
+	               case '(':
+	                   sb.append("\\28");
+	                   break;
+	               case ')':
+	                   sb.append("\\29");
+	                   break;
+	               case '\u0000':
+	                   sb.append("\\00");
+	                   break;
+	               default:
+	                   sb.append(curChar);
+	           }
+	       }
+	       return sb.toString();
+	}
+	
+}
---
+++ src/main/java/au/org/rcc/keycloak/authentication/LinkIdpLoginAuthenticatorFactory.java
@@ -0,0 +1,139 @@
+package au.org.rcc.keycloak.authentication;
+
+import org.keycloak.Config;
+import org.keycloak.authentication.Authenticator;
+import org.keycloak.authentication.AuthenticatorFactory;
+import org.keycloak.authentication.ConfigurableAuthenticatorFactory;
+import org.keycloak.models.AuthenticationExecutionModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.provider.ProviderConfigProperty;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Automatically link the logged in account with an existing
+ * or newly created user rather than prompting the user.
+ *
+ * @author Ilya Kogan
+ */
+public class LinkIdpLoginAuthenticatorFactory implements AuthenticatorFactory, ConfigurableAuthenticatorFactory {
+    public static final String PROVIDER_ID = "hpcportal-link-idp";
+    private static final LinkIdpLoginAuthenticator SINGLETON = new LinkIdpLoginAuthenticator();
+    
+    public static final String LDAP_SERVER_URL = "ldap.server.url";
+    public static final String LDAP_SECURITY_AUTHENTICATION = "ldap.security.authentication";
+    public static final String LDAP_SECURITY_PRINCIPLE = "ldap.security.principle";
+    public static final String LDAP_SECURITY_CREDENTIALS = "ldap.security.credentials";
+    public static final String LDAP_SECURITY_BASE_DN = "ldap.security.base.dn";
+    public static final String CLUSTER_NAMES = "cluster.names";
+    
+    private static AuthenticationExecutionModel.Requirement[] REQUIREMENT_CHOICES = {
+            AuthenticationExecutionModel.Requirement.ALTERNATIVE,
+            AuthenticationExecutionModel.Requirement.DISABLED
+    };
+
+    public String getId() {
+        return PROVIDER_ID;
+    }
+
+    public Authenticator create(KeycloakSession keycloakSession) {
+        return SINGLETON;
+    }
+
+    public void init(Config.Scope scope) {
+
+    }
+
+    public void postInit(KeycloakSessionFactory keycloakSessionFactory) {
+
+    }
+
+    public void close() {
+
+    }
+
+    public String getDisplayType() {
+        return "HPCPortal IDP Login";
+    }
+
+    public String getReferenceCategory() {
+        return "HPCPortal IDP Login";
+    }
+
+    public boolean isConfigurable() {
+        return true;
+    }
+
+    public AuthenticationExecutionModel.Requirement[] getRequirementChoices() {
+        return REQUIREMENT_CHOICES;
+    }
+
+    public boolean isUserSetupAllowed() {
+        return false;
+    }
+
+    public String getHelpText() {
+        return "Automatically link a successful IDP login with any existing (or new) KeyCloak or Federated account.";
+    }
+
+    private static final List<ProviderConfigProperty> configProperties = new ArrayList<ProviderConfigProperty>();
+    static {
+    	//ldap connection url
+        ProviderConfigProperty LdapServerProperty;
+        LdapServerProperty = new ProviderConfigProperty();
+        LdapServerProperty.setName(LDAP_SERVER_URL);
+        LdapServerProperty.setLabel("LDAP Server URL");
+        LdapServerProperty.setType(ProviderConfigProperty.STRING_TYPE);
+        LdapServerProperty.setHelpText("LDAP Server URL");
+        configProperties.add(LdapServerProperty);
+        //security authentication
+        ProviderConfigProperty LdapSecurityAuthentication;
+        LdapSecurityAuthentication = new ProviderConfigProperty();
+        LdapSecurityAuthentication.setName(LDAP_SECURITY_AUTHENTICATION);
+        LdapSecurityAuthentication.setLabel("LDAP Security Authentication");
+        LdapSecurityAuthentication.setType(ProviderConfigProperty.STRING_TYPE);
+        LdapSecurityAuthentication.setDefaultValue("simple");
+        LdapSecurityAuthentication.setHelpText("LDAP Security Authentication");
+        configProperties.add(LdapSecurityAuthentication);
+        //security principle
+        ProviderConfigProperty LdapSecurityPrinciple;
+        LdapSecurityPrinciple = new ProviderConfigProperty();
+        LdapSecurityPrinciple.setName(LDAP_SECURITY_PRINCIPLE);
+        LdapSecurityPrinciple.setLabel("LDAP Security Principle");
+        LdapSecurityPrinciple.setType(ProviderConfigProperty.STRING_TYPE);
+        LdapSecurityPrinciple.setHelpText("LDAP Security Principle");
+        configProperties.add(LdapSecurityPrinciple);
+        //security credentials
+        ProviderConfigProperty LdapSecurityCredentials;
+        LdapSecurityCredentials = new ProviderConfigProperty();
+        LdapSecurityCredentials.setName(LDAP_SECURITY_CREDENTIALS);
+        LdapSecurityCredentials.setLabel("LDAP Security Credentials");
+        LdapSecurityCredentials.setType(ProviderConfigProperty.PASSWORD);
+        LdapSecurityCredentials.setHelpText("LDAP Security Credentials");
+        configProperties.add(LdapSecurityCredentials);
+        //basedn 
+        ProviderConfigProperty LdapBaseDN;
+        LdapBaseDN = new ProviderConfigProperty();
+        LdapBaseDN.setName(LDAP_SECURITY_BASE_DN);
+        LdapBaseDN.setLabel("LDAP User Base DN");
+        LdapBaseDN.setType(ProviderConfigProperty.STRING_TYPE);
+        LdapBaseDN.setHelpText("LDAP User Base DN");
+        configProperties.add(LdapBaseDN);
+        //clusternames 
+        ProviderConfigProperty ClusterNames;
+        ClusterNames = new ProviderConfigProperty();
+        ClusterNames.setName(CLUSTER_NAMES);
+        ClusterNames.setLabel("Cluster names");
+        ClusterNames.setType(ProviderConfigProperty.STRING_TYPE);
+        ClusterNames.setHelpText("Cluser names - separated by semicolon");
+        configProperties.add(ClusterNames);
+    }
+    
+    public List<ProviderConfigProperty> getConfigProperties() {
+        return configProperties;
+    }
+    
+    
+}
