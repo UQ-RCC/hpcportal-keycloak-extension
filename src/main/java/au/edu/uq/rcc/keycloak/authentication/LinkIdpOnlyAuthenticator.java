2798ebbdf4728018ff2a428d9464d2315a35f2da
Zane van Iperen <z.vaniperen@uq.edu.au>
2020-07-24 14:28:46 +1000

    fix packages (again)

--- src/main/java/au/edu/uq/rcc/keycloak/authentication/LinkIdpOnlyAuthenticator.java
+++ src/main/java/au/edu/uq/rcc/keycloak/authentication/LinkIdpOnlyAuthenticator.java
@@ -1,4 +1,4 @@
-package au.org.rcc.keycloak.authentication;
+package au.edu.uq.rcc.keycloak.authentication;
 
 import org.jboss.logging.Logger;
 import org.keycloak.authentication.AuthenticationFlowContext;
